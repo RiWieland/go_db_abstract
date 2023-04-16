bc840570cb74251589fe905b67caf6af5086fc25
wielandos <rich.wieland73@gmail.com>
2023-04-16 15:08:17 +0200

    added body for read in func

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/csv"
 	//"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"os"
+	"path"
 )
 
 type data_reader interface {
@@ -22,19 +25,46 @@ type customer struct {
 
 func main() {
 
+	fileExtension := path.Ext(filePath)
+	if fileExtension == ".csv" {
+
+	} else if fileExtension == ".json" {
+
+	}
+
 }
 
-func readCsvFile(filePath string) [][]string {
+func readCsvFile(filePath string) customer {
+	var records customer
 	f, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal("Unable to read input file "+filePath, err)
 	}
 	defer f.Close()
 
-	csvReader := csv.NewReader(f)
-	records, err := csvReader.ReadAll()
+	fileExtension := path.Ext(filePath)
+	if fileExtension == ".csv" {
+		csvReader := csv.NewReader(f)
+		records, err := csvReader.ReadAll()
+		if err != nil {
+			log.Fatal("Unable to parse file as CSV for "+filePath, err)
+		}
+
+		return records
+	}
+}
+
+func readJsonFile(filePath string) customer {
+	var records customer
+	content, err := ioutil.ReadFile("./config.json")
 	if err != nil {
-		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+		log.Fatal("Error when opening file: ", err)
+	}
+
+	// Now let's unmarshall the data into `payload`
+	err = json.Unmarshal(content, records)
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
 	}
 
 	return records
