dad7d6762f516ad59ac0cb4f0eacd6dde9b57473
wielandos <rich.wieland73@gmail.com>
2023-04-16 14:44:55 +0200

    added interface and struct for customer

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+type data_reader interface {
+	reader()
+}
+
+type customer struct {
+	id          int
+	name        string
+	age         int
+	email       string
+	adress      string
+	phonenumber string
+}
