0418d13f9a140d5dc7a518a8235ddc02c39c16de
wielandos <rich.wieland73@gmail.com>
2023-04-26 17:37:47 +0200

    added struct for customer address

--- main.go
+++ main.go
@@ -15,26 +15,31 @@ type data_reader interface {
 	reader()
 }
 
+type customer_address struct {
+	street string
+	city   string
+	state  string
+}
+
 type customer struct {
-	id          int
-	name        string
-	age         int
-	email       string
-	adress      string
-	phonenumber string
+	id      int
+	name    string
+	age     int
+	address customer_address
 }
 
 func main() {
 	filePath := "data_csv/customer_20230415.csv"
-	fileExtension := path.Ext(filePath)
+	filePathJson := "data_json/customer_20230412.json"
+	fileExtension := path.Ext(filePathJson)
 	if fileExtension == ".csv" {
-		extract := readCsvFile(filePath)
-		fmt.Println(extract)
+		//extract := readCsvFile(filePath)
+		extract_json := readJsonFile(filePath)
+		fmt.Println(extract_json)
 	} else if fileExtension == ".json" {
-		fmt.Printf("not implemented yet")
-
+		extract_json := readJsonFile(filePath)
+		fmt.Println(extract_json)
 	}
-
 }
 
 func readCsvFile(filePath string) []customer {
@@ -58,7 +63,7 @@ func readCsvFile(filePath string) []customer {
 		if err != nil {
 			log.Fatal("can not convert to int: ", age_int, err)
 		}
-		records_customer = append(records_customer, customer{age_int, data[1], 7, data[2], "", ""})
+		records_customer = append(records_customer, customer{age_int, data[1], 7, customer_address{"", "", ""}})
 		fmt.Println(records_customer)
 	}
 	return records_customer
@@ -66,13 +71,13 @@ func readCsvFile(filePath string) []customer {
 
 func readJsonFile(filePath string) customer {
 	var records customer
-	content, err := ioutil.ReadFile("./config.json")
+	content, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal("Error when opening file: ", err)
 	}
 
 	// Now let's unmarshall the data into `payload`
-	err = json.Unmarshal(content, records)
+	err = json.Unmarshal(content, &records)
 	if err != nil {
 		log.Fatal("Error during Unmarshal(): ", err)
 	}
